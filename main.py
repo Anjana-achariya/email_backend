aeb68e70bbc994608c80abb511f9771d7f5cade6
Anjana-achariya <131129025+Anjana-achariya@users.noreply.github.com>
2025-12-07 21:06:11 +0530

    Add generate router to FastAPI application

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from fastapi import FastAPI
-from app.api.v1.match import router as match_router
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.api.v1.match import router as match_router
+from app.api.v1.generate import router as generate_router
 
 app = FastAPI(
     title="SkillSculpt AI Backend",
     version="1.0.0"
 )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],       
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.get("/")
 def health():
@@ -20,3 +24,4 @@ def health():
 
 app.include_router(match_router, prefix="/api/v1")
 
+app.include_router(generate_router, prefix="/api/v1")
